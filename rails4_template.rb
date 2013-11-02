4cfb0b66509bc2a1321a8f26b23bc7f117e27f29
Yutaka Tachibana <yutaka.paradigm.shift@gmail.com>
2013-11-02 16:00:30 +0900

    Add many functions.

--- rails4_template.rb
+++ rails4_template.rb
@@ -5,64 +5,122 @@
 repo_url = "https://raw.github.com/saboyutaka/rails4_template/master"
 
 @app_name = app_name
-
-def get_and_gsub(source_path, local_path)
-  get source_path, local_path
-
-  gsub_file local_path, /%app_name%/, @app_name
-  gsub_file local_path, /%app_name_classify%/, @app_name.classify
-end
+@app_name_upcase = app_name.upcase
 
 # Gemfile
 remove_file 'Gemfile'
 get "#{repo_url}/Gemfile", "Gemfile"
 
 # Bundle install
-run "bundle install"
+# run "bundle install --binstubs='.bundle/bin' --path='./vendor/bundle'"
+
+application  <<-GENERATORS
+    config.generators do |g|
+     g.helper       false
+     g.jbuilder     false
+     g.assets       false
+     g.test_framework :rspec, view_specs: false, request_specs: false
+   end
+GENERATORS
+
+dev =  <<-CODE
+  if ENV['#{@app_name_upcase}_LETTER_OPENER_ENABLE']
+    config.action_mailer.delivery_method = :letter_opener
+  else
+    # configure for mailcatcher
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "localhost",
+      port: (ENV['#{@app_name_upcase}_DEV_SMTP_PORT'] || 1025).to_i
+    }
+  end
+  config.action_mailer.default_url_options = { host: "#{@app_name}.dev" }
+
+  Slim::Engine.set_default_options pretty: true, sort_attrs: false
+CODE
+environment dev, env: 'development'
 
 # use Rspec instead of TestUnit
 remove_dir 'test'
 
 # .gitignore
 remove_file '.gitignore'
-get "#{repo_url}/gitignore", '.gitignore'
+get "#{repo_url}/.gitignore", '.gitignore'
 
 # erb to slim
 remove_file "app/views/layouts/application.html.erb"
 get "#{repo_url}/application.html.slim", 'app/views/layouts/application.html.slim'
 
+# Guardfile
+get "#{repo_url}/Guardfile", 'Guardfile'
+
 # .rubocop.yml
-get "#{repo_url}/rubocop.yml", '.rubocop.yml'
+get "#{repo_url}/.rubocop.yml", '.rubocop.yml'
 
+# database.yml
+remove_file "config/database.yml"
 
-# config/application.rb
-# insert_into_file "config/application.rb",
-#                  %(    config.autoload_paths += Dir[Rails.root.join('lib')]\n),
-#                  after: "# Custom directories with classes and modules you want to be autoloadable.\n"
+file 'config/database.yml', <<-CODE
+  common: &common
+    adapter: mysql2
+    encoding: utf8
+    pool: 5
+    username: <%= ENV['#{@app_name_upcase}_DBUSER'] || 'root' %>
+    password: <%= ENV['#{@app_name_upcase}_DBPASS'] %>
+    host: <%= ENV['#{@app_name_upcase}_DBHOST'] || 'localhost' %>
+    port: <%= ENV['#{@app_name_upcase}_DBPORT'] %>
 
-# insert_into_file "config/application.rb",
-#                  %(    config.i18n.default_locale = :ja\n),
-#                  after: "# config.i18n.default_locale = :de\n"
+  development:
+    <<: *common
+    database: #{@app_name}_development
 
-# insert_into_file "config/application.rb",
-#                  %(    config.autoload_paths += Dir[Rails.root.join('app', 'models')]\n),
-#                  after: "# Custom directories with classes and modules you want to be autoloadable.\n"
+  test:
+    <<: *common
+    database: #{@app_name}_test
 
-# insert_into_file "config/application.rb",
-#                  %(    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]\n),
-#                  after: "# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.\n"
+  production:
+    <<: *common
+    database: #{@app_name}_production
+CODE
 
-# insert_into_file "config/environments/production.rb",
-#                  %(  config.action_controller.page_cache_directory = Rails.root.join("public", "system", "cache")\n),
-#                  after: "# config.cache_store = :mem_cache_store\n"
+# Initializers
 
-# insert_into_file "config/environments/production.rb",
-#                  %(  config.assets.static_cache_control = "public, max-age=#{60 * 60 * 24}"\n),
-#                  after: "# config.cache_store = :mem_cache_store\n"
+initializer 'better_errors.rb', <<-CODE
+  BetterErrors.editor = :sublime if Rails.env.development? && ENV['#{@app_name_upcase}_BETTER_ERRORS_EDITOR_SUBLIME']
+CODE
 
+initializer 'bullet.rb', <<-CODE
+  if Rails.env.development?
+    Rails.application.config.after_initialize do
+      Bullet.enable = true
+      Bullet.alert = (ENV['#{@app_name_upcase}_BULLET_JS_ALERT'] = '1')
+      Bullet.console = true
+      Bullet.bullet_logger = true
+      Bullet.rails_logger = true
+    end
+  end
+CODE
+
+initializer 'mini_profiler.rb', <<-CODE
+  if ENV['#{@app_name_upcase}_PROFILER_DISABLE'] == '1'
+    Rack::MiniProfiler.config.auto_inject = false
+  elsif ENV['#{@app_name_upcase}_PROFILER_HIDDEN'] != '1'
+    Rack::MiniProfiler.config.start_hidden = true
+  end
+CODE
 
 # rspec
 generate 'rspec:install'
+
+if yes?("Do you want to use POW?")
+  file '.powenv', <<-CODE
+    ENV['#{@app_name_upcase}_BETTER_ERRORS_EDITOR_SUBLIME'] = '1'
+    ENV['#{@app_name_upcase}_BULLET_JS_ALERT'] = '1'
+    ENV['#{@app_name_upcase}_PROFILER_DISABLE'] == '1'
+  CODE
+
+  run "powder link"
+end
 
 #
 # Git
@@ -70,4 +128,3 @@ generate 'rspec:install'
 git :init
 git :add => '.'
 git :commit => '-am "Initial commit"'
-
