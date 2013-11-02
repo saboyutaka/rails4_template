53e9b5b528de01e0e6903c8231cfc7cb08594f21
Yutaka Tachibana <yutaka.paradigm.shift@gmail.com>
2013-11-02 17:24:13 +0900

    Fix template file.

--- rails4_template.rb
+++ rails4_template.rb
@@ -13,30 +13,31 @@ get "#{repo_url}/Gemfile", "Gemfile"
 
 # Bundle install
 # run "bundle install --binstubs='.bundle/bin' --path='./vendor/bundle'"
+run "bundle install"
 
-application  <<-GENERATORS
-    config.generators do |g|
-     g.helper       false
-     g.jbuilder     false
-     g.assets       false
-     g.test_framework :rspec, view_specs: false, request_specs: false
-   end
+application <<GENERATORS
+config.generators do |g|
+   g.helper       false
+   g.jbuilder     false
+   g.assets       false
+   g.test_framework :rspec, view_specs: false, request_specs: false
+ end
 GENERATORS
 
-dev =  <<-CODE
-  if ENV['#{@app_name_upcase}_LETTER_OPENER_ENABLE']
-    config.action_mailer.delivery_method = :letter_opener
-  else
-    # configure for mailcatcher
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address: "localhost",
-      port: (ENV['#{@app_name_upcase}_DEV_SMTP_PORT'] || 1025).to_i
-    }
-  end
-  config.action_mailer.default_url_options = { host: "#{@app_name}.dev" }
+dev = <<-CODE
+if ENV['#{@app_name_upcase}_LETTER_OPENER_ENABLE']
+  config.action_mailer.delivery_method = :letter_opener
+else
+  # configure for mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "localhost",
+    port: (ENV['#{@app_name_upcase}_DEV_SMTP_PORT'] || 1025).to_i
+  }
+end
+config.action_mailer.default_url_options = { host: "#{@app_name}.dev" }
 
-  Slim::Engine.set_default_options pretty: true, sort_attrs: false
+Slim::Engine.set_default_options pretty: true, sort_attrs: false
 CODE
 environment dev, env: 'development'
 
@@ -60,53 +61,41 @@ get "#{repo_url}/.rubocop.yml", '.rubocop.yml'
 # database.yml
 remove_file "config/database.yml"
 
-file 'config/database.yml', <<-CODE
-  common: &common
-    adapter: mysql2
-    encoding: utf8
-    pool: 5
-    username: <%= ENV['#{@app_name_upcase}_DBUSER'] || 'root' %>
-    password: <%= ENV['#{@app_name_upcase}_DBPASS'] %>
-    host: <%= ENV['#{@app_name_upcase}_DBHOST'] || 'localhost' %>
-    port: <%= ENV['#{@app_name_upcase}_DBPORT'] %>
+file 'config/database.yml', <<CODE
+common: &common
+  adapter: mysql2
+  encoding: utf8
+  pool: 5
+  username: <%= ENV['#{@app_name_upcase}_DBUSER'] || 'root' %>
+  password: <%= ENV['#{@app_name_upcase}_DBPASS'] %>
+  host: <%= ENV['#{@app_name_upcase}_DBHOST'] || 'localhost' %>
+  port: <%= ENV['#{@app_name_upcase}_DBPORT'] %>
 
-  development:
-    <<: *common
-    database: #{@app_name}_development
+development:
+  <<: *common
+  database: #{@app_name}_development
 
-  test:
-    <<: *common
-    database: #{@app_name}_test
+test:
+  <<: *common
+  database: #{@app_name}_test
 
-  production:
-    <<: *common
-    database: #{@app_name}_production
+production:
+  <<: *common
+  database: #{@app_name}_production
 CODE
 
 # Initializers
 
-initializer 'better_errors.rb', <<-CODE
-  BetterErrors.editor = :sublime if Rails.env.development? && ENV['#{@app_name_upcase}_BETTER_ERRORS_EDITOR_SUBLIME']
+initializer 'better_errors.rb', <<CODE
+BetterErrors.editor = :sublime if Rails.env.development? && ENV['#{@app_name_upcase}_BETTER_ERRORS_EDITOR_SUBLIME']
 CODE
 
-initializer 'bullet.rb', <<-CODE
-  if Rails.env.development?
-    Rails.application.config.after_initialize do
-      Bullet.enable = true
-      Bullet.alert = (ENV['#{@app_name_upcase}_BULLET_JS_ALERT'] = '1')
-      Bullet.console = true
-      Bullet.bullet_logger = true
-      Bullet.rails_logger = true
-    end
-  end
-CODE
-
-initializer 'mini_profiler.rb', <<-CODE
-  if ENV['#{@app_name_upcase}_PROFILER_DISABLE'] == '1'
-    Rack::MiniProfiler.config.auto_inject = false
-  elsif ENV['#{@app_name_upcase}_PROFILER_HIDDEN'] != '1'
-    Rack::MiniProfiler.config.start_hidden = true
-  end
+initializer 'mini_profiler.rb', <<CODE
+if ENV['#{@app_name_upcase}_PROFILER_DISABLE'] == '1'
+  Rack::MiniProfiler.config.auto_inject = false
+elsif ENV['#{@app_name_upcase}_PROFILER_HIDDEN'] != '1'
+  Rack::MiniProfiler.config.start_hidden = true
+end
 CODE
 
 # rspec
@@ -114,10 +103,9 @@ generate 'rspec:install'
 
 if yes?("Do you want to use POW?")
   file '.powenv', <<-CODE
-    ENV['#{@app_name_upcase}_BETTER_ERRORS_EDITOR_SUBLIME'] = '1'
-    ENV['#{@app_name_upcase}_BULLET_JS_ALERT'] = '1'
-    ENV['#{@app_name_upcase}_PROFILER_DISABLE'] == '1'
-  CODE
+export #{@app_name_upcase}_BETTER_ERRORS_EDITOR_SUBLIME='1'
+export #{@app_name_upcase}_PROFILER_DISABLE='1'
+CODE
 
   run "powder link"
 end
